1d980e1146b094061b0e0e149ea7536f8a6b59c5
sriramgreen <sriram.k@qbrainx.com>
2025-07-25 19:25:06 +0530

    agent_screen_api_integrateion

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,13 @@ import Foundation
 import cloud_firestore
 import file_selector_macos
 import firebase_auth
+import firebase_auth
 import firebase_core
 import firebase_storage
+import firebase_storage
 import flutter_tts
+import shared_preferences_foundation
+import path_provider_foundation
 import shared_preferences_foundation
 import speech_to_text_macos
 
@@ -21,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
